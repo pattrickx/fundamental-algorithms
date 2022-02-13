e9ab8787f69fcfeca1aeeb10dc15ae3d9dae52c4
pattrickx <pattrickx@edu.unifor.br>
2022-02-13 01:02:22 -0300

    PrimeCard

---
+++ src/components/PrimeCard/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import "./styles.css"
+
+function PrimeCard() {
+  const [number,setNumber]= useState(null)
+  const [resultMessenger,setResultMessenger]= useState("")
+
+  function Prime(){
+    
+    for(let i=2; i<number ; i++)
+      if(number%i===0)
+        return false
+    
+    return number>1
+
+  }
+  
+  function handleClick(){
+    const IsPrime = Prime()
+    setResultMessenger(IsPrime?"Is Prime":"Is Not Prime")
+  }
+
+  return (
+    <div className="container">
+      <h1>Prime Number</h1>
+      <div className='inputContainer'>
+        <input type="number" value={number} onChange={event=>setNumber(event.target.value)} />
+        <button onClick={()=>handleClick()}>
+          RUN
+        </button>
+      </div>
+        
+      <label className='result'>{resultMessenger}</label>
+    </div>
+  );
+}
+
+export default PrimeCard;
---
+++ src/pages/App.js
@@ -0,0 +1,18 @@
+import PrimeCard from '../components/PrimeCard';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <PrimeCard/>
+      <PrimeCard/>
+      <PrimeCard/>
+      <PrimeCard/>
+      <PrimeCard/>
+      <PrimeCard/>
+    </div>
+  );
+}
+
+export default App;
