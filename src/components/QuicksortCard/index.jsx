c4421f674766cd44a6234df701dbac2effd2bbc3
pattrickx <pattrickx@edu.unifor.br>
2022-02-14 17:11:12 -0300

    add QuicksortCard

---
+++ src/components/QuicksortCard/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function QuicksortCard() {
+    const [inputString,setInputString]= useState("")
+    const [outputArray,setOutputArray]= useState("")
+    function Quicksort(){
+        const inputArray = inputString.split(",")
+
+
+        
+
+        setOutputArray("")
+    }
+    return (
+        <div className="container">
+        <h1>Quicksort</h1>
+        <div className='inputContainer'>
+            <input type="text" value={inputString} onChange={event=>setInputString(event.target.value)} />
+            <button onClick={()=>Quicksort()}>
+                RUN
+            </button>
+        </div>
+        
+        <label className='resultQuicksort'>{outputArray}</label>
+    </div>);
+}
+
+export default QuicksortCard;
