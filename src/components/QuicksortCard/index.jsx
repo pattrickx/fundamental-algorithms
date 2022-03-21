a9ab337ea598ee88eac45d49e16650d51f07627f
pattrickx <pattrickx@edu.unifor.br>
2022-03-21 20:51:16 -0300

    Quicksort

--- src/components/QuicksortCard/index.jsx
+++ src/components/QuicksortCard/index.jsx
@@ -5,20 +5,41 @@ import React, { useState } from 'react';
 function QuicksortCard() {
     const [inputString,setInputString]= useState("")
     const [outputArray,setOutputArray]= useState("")
-    function Quicksort(){
-        const inputArray = inputString.split(",")
+
+    function getList(){
+        const listNumber = inputString.split(",")
+                                        .filter(x=>!isNaN(x) && x!=" " && x!="")
+                                        .map(x=>{return parseFloat(x)})
+        return listNumber
+    }
 
 
+    function Quicksort(listNumber){
         
+        if (listNumber.length <=1)
+            return listNumber
+        let pivot= listNumber[0]
+        let left = []
+        let center = []
+        let rigth = []
+        for( let n of listNumber ){
+            if(n<pivot) left.push(n)
+            if(n>pivot) rigth.push(n)
+            if(n==pivot) center.push(n)
+        }
 
-        setOutputArray("")
+        return[...Quicksort(left),...center,...Quicksort(rigth)]
+    }
+    function handleClick(){
+
+        setOutputArray(Quicksort(getList()).join(', '))
     }
     return (
         <div className="container">
         <h1>Quicksort</h1>
         <div className='inputContainer'>
             <input type="text" value={inputString} onChange={event=>setInputString(event.target.value)} />
-            <button onClick={()=>Quicksort()}>
+            <button onClick={()=>handleClick()}>
                 RUN
             </button>
         </div>
